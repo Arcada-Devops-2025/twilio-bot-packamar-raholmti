ca8f6bf73124dd91c77af8b8097382610081439f
tintin.raholm <tintin.raholm@arcada.fi>
2025-10-15 09:30:40 +0300

    app.listen

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const { twilio } = require('twilio')
+const PORT = 3000
 
 const app = express()
 
 app.post('/voicecall', (req, res) => {
     const response = new twilio.VoiceResponse()
+    twilio.say('Hello')
+
+    response.type('text/xml')
+    response.send(twilio.toString())
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`)
 })
