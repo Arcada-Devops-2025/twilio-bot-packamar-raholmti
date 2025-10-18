66526702a0d3e6ec544e94b4e6fd3737f24610cb
tintin.raholm <tintin.raholm@arcada.fi>
2025-10-18 21:45:15 +0300

    download to ec2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.post('/voicecall', (req, res) => {
     const twiml = new VoiceResponse()
-    twiml.say('Hello!')
+    twiml.say('Hello!!')
 
     res.type('text/xml')
     res.send(twiml.toString())
